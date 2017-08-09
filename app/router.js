8ca06d0888106a4a873845df622461be5b269000
Sowmya Logaswamy <sowmyalogaswamy@gmail.com>
2017-08-09 12:05:08 -0700

    create index, about and contact routes

--- app/router.js
+++ app/router.js
@@ -1,12 +1,15 @@
-import Ember from 'ember';
-import config from './config/environment';
+//router.js file is created by the Ember CLI
+import Ember from 'ember'; //Access to Ember.js library as the variable Ember
+import config from './config/environment'; // Access to our app's configuration data as the word config
 
-const Router = Ember.Router.extend({
+const Router = Ember.Router.extend({ //defines a constant Router whose value cant be changed
   location: config.locationType,
   rootURL: config.rootURL
 });
 
 Router.map(function() {
+  this.route('about');
+  this.route('contact');
 });
 
-export default Router;
+export default Router; //makes router constant available to other parts of app
